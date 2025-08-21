66216df3a55933227deb4371b9c8d6e962dedea9
Asraar Ahmed Contractor <124158571+Racdf@users.noreply.github.com>
2025-08-21 15:22:12 +0530

    adjusted position by 20%

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -5,7 +5,7 @@ const COLORS = ['#A000FF', '#FF9304', '#FDE006', '#69D3FF'];
 
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent }) => {
-  const radius = outerRadius * 0.5;  
+  const radius = outerRadius * 0.70;  
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
