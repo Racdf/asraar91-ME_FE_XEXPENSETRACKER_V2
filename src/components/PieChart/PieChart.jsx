c6e44201c729dc96b6b595e1e0a870fc1e19a937
Asraar Ahmed Contractor <124158571+Racdf@users.noreply.github.com>
2025-08-21 15:15:30 +0530

    label fix

--- src/components/PieChart/PieChart.jsx
+++ src/components/PieChart/PieChart.jsx
@@ -4,17 +4,26 @@ import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 const COLORS = ['#A000FF', '#FF9304', '#FDE006', '#69D3FF'];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent }) => {
+  const radius = outerRadius * 0.5;  
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-    return (
-        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-            {`${(percent * 100).toFixed(0)}%`}
-        </text>
-    );
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor="middle"
+      dominantBaseline="central"
+      fontSize={14}
+      fontWeight="bold"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
 };
+
 
 export default function PieChartComponent({data}) {
     return (
